c41e218325b967242c6975d5163845dc0fe5e28a
unknown <haliercesr@gmail.com>
2024-08-28 16:18:25 -0300

    se agrego async y await

--- app.js
+++ app.js
@@ -6,36 +6,40 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const website = 'https://www.amazon.es/dp/B09TP2B11R/';
+const website = 'https://amazon.es/dp/B07M98CL6Y/';
 
 try {
-  
-  axios(website).then((res) => {
-    const data = res.data;
-    const $ = cheerio.load(data);
 
-    let content = [];
+  async function scraper() {
+    await axios(website).then((res) => {
+      const data = res.data;
+      const $ = cheerio.load(data);
+      console.log("valor de cherio"+$)
+      let content = [];
 
       $('.a-size-large', data).each(function () {
-      const title = $(this).text();
-      const url = $(this).find('a').attr('href');
-      content.push({
-        title,
+        const title = $(this).text();
+        const url = $(this).find('a').attr('href');
+        content.push({
+          title,
+        });
       });
-    });
 
-    $('.a-list-item', data).each(function () {
-      const features = $(this).text();
-      content.push({
-        features,
+      $('.a-list-item', data).each(function () {
+        const features = $(this).text();
+        content.push({
+          features,
+        });
       });
-    });
 
-    app.get('/', (req, res) => {
-      res.status(200).json(content);
-    });
+      app.get('/', (req, res) => {
+        res.status(200).json(content);
+      });
 
-  });
+    });
+  }
+
+  scraper();
 } catch (error) {
   console.log(error, error.message);
 }

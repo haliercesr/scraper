108b62f18abe6618dd004b075683c4ea6ec545bc
HalierC <115671323+haliercesr@users.noreply.github.com>
2024-09-12 15:22:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -276,33 +276,11 @@ server.use(logger('dev'));
 
 const PORT = process.env.PORT || 3000;
 const maxRetries = 5; // Número máximo de reintentos
-function generateHeaders() {
-  const chromeVersions = ['91', '92', '93', '94', '95'];
-  const randomChromeVersion = chromeVersions[Math.floor(Math.random() * chromeVersions.length)];
-  
-  return {
-    "User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${randomChromeVersion}.0.${Math.floor(Math.random() * 9999)}.0 Safari/537.36`,
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-User": "?1",
-    "Sec-Fetch-Dest": "document",
-    "Cache-Control": "max-age=0",
-    "sec-ch-ua": `"Google Chrome";v="${randomChromeVersion}", "Chromium";v="${randomChromeVersion}", ";Not A Brand";v="99"`,
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "DNT": "1",
-    "Pragma": "no-cache"
-  };
-}
+
 
 // Función para hacer scraping con reintento
 async function scraper(website, retries = 0) {
-  const headers = generateHeaders();
+  const headers = userAgentsfunction();
   try {
     const { data } = await axios.get(website, { headers, timeout: 20000 }); // Configura un header y un timeout de 5 segundos
     const $ = cheerio.load(data);

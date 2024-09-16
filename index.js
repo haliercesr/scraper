1c3fef0e11f69b86b8dfb2b15845f1d2c4511f90
HalierC <115671323+haliercesr@users.noreply.github.com>
2024-09-16 11:53:19 -0300

    Update index.js V1

--- index.js
+++ index.js
@@ -1,356 +1,107 @@
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
-const logger = require('morgan');
-const cors = require('cors');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms)); // Función para generar retrasos
 
-function userAgentsfunction() {
-  const headersArray = [
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:116.0) Gecko/20100101 Firefox/116.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1; rv:116.0) Gecko/20100101 Firefox/116.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:116.0) Gecko/20100101 Firefox/116.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1; rv:115.0) Gecko/20100101 Firefox/115.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:115.0) Gecko/20100101 Firefox/115.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Mozilla Firefox on Windows
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:116.0) Gecko/20100101 Firefox/116.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Mozilla Firefox on MacOS
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1; rv:116.0) Gecko/20100101 Firefox/116.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Mozilla Firefox on Ubuntu
-    {
-      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:116.0) Gecko/20100101 Firefox/116.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Google Chrome on Windows
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Google Chrome on MacOS
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Google Chrome on Ubuntu
-    {
-      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Microsoft Edge on Windows
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.0.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Microsoft Edge on MacOS
-    {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.0.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-    // Opera on Windows
-    {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/100.0.0.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0"
-    },
-  ]
-
-  // select a random header from the list
-  const header = headersArray[Math.floor(Math.random() * headersArray.length)];
-
-  return header
+// Función para rotar User-Agents aleatoriamente
+function getRandomUserAgent() {
+  const userAgents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Safari/605.1.15',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A5341f Safari/604.1',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    // Agrega más User-Agents si es necesario
+  ];
+  return userAgents[Math.floor(Math.random() * userAgents.length)];
 }
+
+// Función para generar cabeceras aleatorias para cada solicitud
+function userAgentsfunction() {
+  const headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'es-ES,es;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.5',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': getRandomUserAgent(), // Usamos un User-Agent aleatorio
+    // 'proxy': 'http://proxy-ip:proxy-port' // (Opcional) Si quieres usar proxies rotativos
+  };
+  return headers;
+}
+
 const server = express();
-
-server.use(cors())
-server.use(logger('dev'));
-
 const PORT = process.env.PORT || 3000;
 const maxRetries = 5; // Número máximo de reintentos
 
-
-// Función para hacer scraping con reintento
+// Función para hacer scraping con reintento y retrasos aleatorios
 async function scraper(website, retries = 0) {
   const headers = userAgentsfunction();
+  console.log('Headers being sent:', headers);
+
   try {
-    const { data } = await axios.get(website, { headers, timeout: 20000 }); // Configura un header y un timeout de 5 segundos
+    const delayTime = Math.floor(Math.random() * 3000) + 2000; // Retraso aleatorio entre 2 y 5 segundos
+    await delay(delayTime); // Introducimos un retraso aleatorio
+
+    const { data } = await axios.get(website, { headers, timeout: 10000 }); // Timeout de 10 segundos
     const $ = cheerio.load(data);
 
-    let content = {
-      title: "",
-      features: [],
-      information: [],
-      contentbox: [],
-      details: [],
-      reviews: [],
-      images: []
-    };
+    let content = [];
 
     // Selector optimizado para capturar el título
     const title = $('#productTitle').text().trim();
     if (title) {
-      content.title = title;
+      content.push({ title });
     }
 
     // Selector optimizado para capturar las características
     $('#feature-bullets .a-list-item').each(function () {
       const feature = $(this).text().trim();
       if (feature) {
-        content.features.push(feature);
+        content.push({ feature });
       }
     });
-    // Selector para capturar texto del div con clase aplus-v2 desktop celwidget
-    $('#aplus .a-spacing-mini').each(function () {
+
+    // Capturar texto adicional y detalles del producto
+    $('#aplus .a-spacing-mini, #aplus .a-spacing-base, #aplus p').each(function () {
       const aplusText = $(this).text().trim();
       if (aplusText) {
-        content.information.push(aplusText);
+        content.push({ type: 'more-information', text: aplusText });
       }
     });
-    $('#aplus .a-spacing-base').each(function () {
-      const aplusText = $(this).text().trim();
-      if (aplusText) {
-        content.information.push(aplusText);
-      }
-    });
-    $('#aplus p').each(function () {
-      const aplusText = $(this).text().trim();
-      if (aplusText) {
-        content.information.push(aplusText);
-      }
-    });
-    // Selector para capturar detalles del producto
+
     $('#detailBullets_feature_div .a-list-item span').each(function () {
       const detailsText = $(this).text().trim();
       if (detailsText) {
-        content.details.push(detailsText);
+        content.push({ type: 'details-product', text: detailsText });
       }
     });
-    // Selector para capturar contenido de la caja
+
+    // Capturar contenido de la caja
     $('#witb-content-list span').each(function () {
       const contentBoxText = $(this).text().trim();
       if (contentBoxText) {
-        content.contentbox.push(contentBoxText);
+        content.push({ type: 'content-box', text: contentBoxText });
       }
     });
-    // Selector para capturar reviews
+
+    // Capturar reviews
     $('#cm-cr-dp-review-list .a-expander-content span').each(function () {
       const reviewText = $(this).text().trim();
       if (reviewText) {
-        content.reviews.push(reviewText);
+        content.push({ type: 'reviews-product', text: reviewText });
       }
     });
-    // Selector para capturar las URLs de las imágenes
-    const images = [];
 
+    // Capturar URLs de las imágenes
+    const images = [];
     const scriptContent = $('script:contains("colorImages")').html();
     if (scriptContent) {
       const matches = scriptContent.match(/"hiRes":"(https:\/\/[^"]+)"/g);
@@ -361,35 +112,36 @@ async function scraper(website, retries = 0) {
         });
       }
     }
-    // Agregar URLs de las imágenes al contenido
-    content.images= images;
+    content.push({ images: images });
 
     return content;
 
   } catch (error) {
-    console.log(error.message)
-    return error
+    if (retries < maxRetries) {
+      console.log(`Error: ${error.message}. Reintentando (${retries + 1}/${maxRetries})`);
+      await delay(3000); // Esperar 3 segundos antes de reintentar
+      return scraper(website, retries + 1); // Reintentar
+    } else {
+      console.log('Se alcanzó el número máximo de reintentos');
+      return { error: 'Se alcanzó el número máximo de reintentos', details: error.message };
+    }
   }
 }
 
 server.get('/agent/user', async (req, res) => {
   const headers = userAgentsfunction();
   try {
-    await axios("https://httpbin.io/user-agent", {
-      headers,
-    })
-      .then(({ data }) => {
-        res.status(200).json(data);
-        console.log(data)
-      });
+    const { data } = await axios("https://httpbin.io/user-agent", { headers });
+    res.status(200).json(data);
+    console.log(data);
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
 });
 
 server.get('/:id', async (req, res) => {
   try {
-    const cp = req.params.id
+    const cp = req.params.id;
     const website = `https://amazon.es/dp/${cp}/`;
     const content = await scraper(website);
     res.status(200).json(content);
@@ -399,8 +151,13 @@ server.get('/:id', async (req, res) => {
   }
 });
 
-
-
 server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+server.use((req, res, next) => {
+  console.log('Incoming request headers:', req.headers);
+  next();
+});
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
